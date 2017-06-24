03e81660538c4f543a7bc54b76612607d7397793
Marlene Oliveira <marleneoliveira.w@gmail.com>
2017-06-25 00:23:27 +0100

    - The server now checks if the database is available.

--- chirper-server/server.js
+++ chirper-server/server.js
@@ -1,8 +1,25 @@
 var express = require('express'),
-y-  config = require('./configuration/configuration.js'),
+  config = require('./configuration/configuration.js'),
   app = express(),
   port = config.SERVER_PORT;
 
+var MongoClient = require('mongodb').MongoClient;
+
+// check the server connection
+MongoClient.connect(config.MONGO_URL, function (err, db) {
+  if (db === null) {
+    console.log(config.MONGO_URL + " not available!");
+  } else {
+    if (err === null) {
+      console.log("Connected successfully to server");
+    } else {
+      console.log("Error connecting to server");
+    }
+    db.close();
+  }
+});
+
+
 app.listen(port);
 
-console.log('chirper server running on port: ' + port);
+console.log('chirper back-end server running on port: ' + port);
