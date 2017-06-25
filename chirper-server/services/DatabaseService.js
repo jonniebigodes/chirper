d9ac58dea3e04511a98a31f20c7f2537d952ccbd
Marlene Oliveira <marleneoliveira.w@gmail.com>
2017-06-25 23:36:38 +0100

    - Created structure of the DatabaseService. - Created the testConnection function, that checks if the selected database is up.

--- chirper-server/services/DatabaseService.js
+++ chirper-server/services/DatabaseService.js
@@ -1,3 +1,35 @@
 /**
  * Handles the communication with the database
  */
+
+module.exports = {
+    /**
+     * Test the connection to the database.
+     * @param {*} url the database url
+     */
+    testConnection: function (url, MongoClient) {
+        // check the server connection
+        
+        MongoClient.connect(url, function (err, db) {
+            
+            if (db === null) {
+                console.log("Database " + url + " not available!");
+            } else {
+                if (err !== null) {
+                    console.log("Database Test Not OK: Cannot Connect.");
+                } else {
+                    console.log("Database Test OK: Connected successfully.");         
+                }
+                db.close();
+            }
+        });
+    },
+
+    createDatabase: function () {
+
+    },
+
+    createCollections: function () {
+
+    }
+}
