694f6c39232238ba4192f947c2af369db7307b73
Marlene Oliveira <marleneoliveira.w@gmail.com>
2017-06-25 23:47:37 +0100

    - Moved the MongoClient definition to the DatabaseService. - Added functions that store an active database connection, get it and set it.

--- chirper-server/services/DatabaseService.js
+++ chirper-server/services/DatabaseService.js
@@ -2,12 +2,40 @@
  * Handles the communication with the database
  */
 
+var MongoClient = require('mongodb').MongoClient;
+
+var _database;
+
 module.exports = {
+    /**
+     * Returns the database connection object.
+     */
+    getDatabaseConnection: function(){
+        return _database;
+    },
+
+    /**
+     * Sets the database connection object.
+     */
+    setDatabaseConnection: function(db){
+        _database = db;
+    },
+    /**
+     * Getting the database object:
+     * https://stackoverflow.com/questions/24621940/how-to-properly-reuse-connection-to-mongodb-across-nodejs-application-and-module
+     */
+    connectToDatabase: function(url, callback){
+        MongoClient.connect(url, function (err, db) {
+            _database = db;
+            return callback(err);
+        });
+    },
+
     /**
      * Test the connection to the database.
      * @param {*} url the database url
      */
-    testConnection: function (url, MongoClient) {
+    testConnection: function (url) {
         // check the server connection
         
         MongoClient.connect(url, function (err, db) {
