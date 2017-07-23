5c35b49f2a8cf3dd3642afbb6f9d18d92c93a1eb
Marlene Oliveira <marleneoliveira.w@gmail.com>
2017-07-24 00:34:22 +0100

    - The constants in the configuration.js are now frozen to prevent accidental changes.

--- chirper-server/configuration/configuration.js
+++ chirper-server/configuration/configuration.js
@@ -1,9 +1,10 @@
-module.exports = {
-    SERVER_PORT: 9000,
-    MONGO_URL: "mongodb://localhost:27017",
-    CHIRPER_DB: "/chirperdb",
-    USERS_COLLECTION : "users",
-    SETTINGS_COLLECTION: "settings",
-    POSTS_COLLECTION: "posts",
-    MESSAGES_COLLECTION: "messages"
-}
+module.exports = Object.freeze({
+    SERVER_PORT: Object.freeze(9000),
+    MONGO_URL: Object.freeze("mongodb://localhost:27017/"),
+    CHIRPER_DB: Object.freeze("chirperdb"),
+    USERS_COLLECTION : Object.freeze("users"),
+    SETTINGS_COLLECTION: Object.freeze("settings"),
+    POSTS_COLLECTION: Object.freeze("posts"),
+    MESSAGES_COLLECTION: Object.freeze("messages"),
+    COLLECTIONS_LIST:Object.freeze(["users", "settings","posts", "messages"])
+});
